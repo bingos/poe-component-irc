c87a282f56bfc06ad4bcf9e1ee016b2ecbbb4b4c
Chris Williams <chris@bingosnet.co.uk>
2005-05-28 11:53:38 +0000

    Added ::Test::Plugin to be used in the new test suite

---
+++ lib/POE/Component/IRC/Test/Plugin.pm
@@ -0,0 +1,22 @@
+package POE::Component::IRC::Test::Plugin;
+
+use POE::Component::IRC::Plugin qw( :ALL );
+
+sub new {
+  return bless { @_[1..$#_] }, $_[0];
+}
+
+sub PCI_register {
+  $_[1]->plugin_register( $_[0], 'SERVER', qw(all) );
+  return 1;
+}
+
+sub PCI_unregister {
+  return 1;
+}
+
+sub _default {
+  return PCI_EAT_NONE;
+}
+
+1;
