ddd51333e163c4995a6a9c58bd74a8799cd9492a
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2009-03-04 14:14:48 +0000

    Pod link

--- lib/POE/Component/IRC/Plugin/BotCommand.pm
+++ lib/POE/Component/IRC/Plugin/BotCommand.pm
@@ -222,8 +222,9 @@ to issue commands. Default is true.
 B<'Prefix'>, if B<'Addressed'> is false, all commands must be prefixed with this
 string. Default is '!'. You can set it to '' to allow bare commands.
 
-B<'Eat'>, set to true to make the plugin hide C<irc_public> events from other
-plugins if they contain a valid command. Default is false.
+B<'Eat'>, set to true to make the plugin hide
+C<irc_public|POE::Component::IRC/"irc_public"> events from other plugins if they
+contain a valid command. Default is false.
 
 Returns a plugin object suitable for feeding to
 L<POE::Component::IRC|POE::Component::IRC>'s C<plugin_add> method.
