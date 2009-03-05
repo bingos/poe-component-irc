044290baf4e57208c5dc1fd81471929e213c8227
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2009-03-05 02:18:19 +0000

    Fix RT #43856

--- lib/POE/Component/IRC/Plugin/PlugMan.pm
+++ lib/POE/Component/IRC/Plugin/PlugMan.pm
@@ -97,7 +97,7 @@ sub S_public {
     my $cmd = uc (shift @cmd);
     
     if (defined $self->{commands}->{$cmd}) {
-        $self->{command}->{$cmd}->($self, 'privmsg', $channel, @cmd);
+        $self->{commands}->{$cmd}->($self, 'privmsg', $channel, @cmd);
     }
     
     return PCI_EAT_NONE;
@@ -114,7 +114,7 @@ sub S_msg {
     return PCI_EAT_NONE if !$self->_bot_owner($nick);
     
     if (defined $self->{commands}->{$cmd}) {
-        $self->{command}->{$cmd}->($self, 'notice', $nick, @cmd);
+        $self->{commands}->{$cmd}->($self, 'notice', $nick, @cmd);
     }
 
     return PCI_EAT_NONE;
