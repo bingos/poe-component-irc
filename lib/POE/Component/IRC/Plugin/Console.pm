d9eb28f0ca30917488c05921d1130ab79cef37e5
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2010-10-19 09:13:31 +0000

    Console.pm: Dump hashes and arrays recursively

--- lib/POE/Component/IRC/Plugin/Console.pm
+++ lib/POE/Component/IRC/Plugin/Console.pm
@@ -5,6 +5,7 @@ use warnings FATAL => 'all';
 use Carp;
 use POE qw(Wheel::SocketFactory Wheel::ReadWrite Filter::IRCD Filter::Line Filter::Stackable);
 use POE::Component::IRC::Plugin qw( :ALL );
+use Scalar::Util qw(looks_like_number);
 
 sub new {
     my $package = shift;
@@ -39,6 +40,34 @@ sub PCI_unregister {
     return 1;
 }
 
+sub _dump {
+    my ($arg) = @_;
+
+    if (ref $arg eq 'ARRAY') {
+        my @elems;
+        for my $elem (@$arg) {
+            push @elems, _dump($elem);
+        }
+        return '['. join(', ', @elems) .']';
+    }
+    elsif (ref $arg eq 'HASH') {
+        my @pairs;
+        for my $key (keys %$arg) {
+            push @pairs, [$key, _dump($arg->{$key})];
+        }
+        return '{'. join(', ', map { "$_->[0] => $_->[1]" } @pairs) .'}';
+    }
+    elsif (ref $arg) {
+        return $arg;
+    }
+    elsif (defined $arg) {
+        return looks_like_number($arg) ? $arg : "'$arg'";
+    }
+    else {
+        return 'undef';
+    }
+}
+
 sub _default {
     my ($self, $irc, $event) = splice @_, 0, 3;
     return PCI_EAT_NONE if $event eq 'S_raw';
@@ -47,19 +76,8 @@ sub _default {
     my @args = map { $$_ } @_;
     my @output;
 
-    for my $arg (@args) {
-        if (ref $arg eq 'ARRAY') {
-            push @output, '['. join(', ', map { "'$_'" } @$arg) .']';
-        }
-        elsif (ref $arg eq 'HASH') {
-            push @output, '{'. join(', ', map { "$_ => '$arg->{$_}'" } keys %$arg) .'}';
-        }
-        elsif (defined $arg) {
-            push @output, "'$arg'";
-        }
-        else {
-            push @output, 'undef';
-        }
+    for my $i (0..$#args) {
+        push @output, "ARG$i: " . _dump($args[$i]);
     }
 
     for my $wheel_id ( keys %{ $self->{wheels} } ) {
