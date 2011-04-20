0d242457625554a872dcf6320b250041a39c7725
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2011-04-20 18:20:36 +0000

    Qnet/State.pm: Forgot to import parse_user() from IRC::Utils.

--- lib/POE/Component/IRC/Qnet/State.pm
+++ lib/POE/Component/IRC/Qnet/State.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings FATAL => 'all';
 use Carp;
 use POE;
-use IRC::Utils qw(uc_irc parse_mask);
+use IRC::Utils qw(uc_irc parse_mask parse_user);
 use POE::Component::IRC::Plugin qw(:ALL);
 use base qw(POE::Component::IRC::State POE::Component::IRC::Qnet);
 
